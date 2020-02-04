3a59a515fac81c6a871ba060d2798331881d61e2
wanjka <vantusssss@yandex.ru>
2020-02-04 20:22:46 +0300

    Doing search with a little help of music21

--- pyper/__init__.py
+++ pyper/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template, flash, redirect, url_for
 from pyper.db import db
 from pyper.track.views import bp as track_blueprint
 
+# типа паттерн "фабрика"
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
     db.init_app(app)
+    # блюпринт всего один
     app.register_blueprint(track_blueprint)
     return app
 
